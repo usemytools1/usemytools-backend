a4832ad5bbdedc269cc0af47f99b342008a20508
GregoryUrban <turknben@gmail.com>
2019-07-19 00:54:29 -0400

    Seed heroku issue fixed, added names back into seeds

--- data/seeds/001-users.js
+++ data/seeds/001-users.js
@@ -28,4 +28,21 @@ const cleaner = require("knex-cleaner");
 
 exports.seed = async function (knex) {
   await cleaner.clean(knex);
+  return knex('users').insert([{
+      username: 'David',
+      password: 'test'
+    },
+    {
+      username: 'Greg',
+      password: 'test'
+    },
+    {
+      username: 'Carlos',
+      password: 'test'
+    },
+    {
+      username: 'Chaz',
+      password: 'test'
+    }
+  ]);
 };
