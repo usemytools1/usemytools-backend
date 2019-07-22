30bb7430e97cfc54351c8ded37396de007d2ee36
David <digital0david@gmail.com>
2019-07-22 18:39:29 -0400

    updated seeds

--- data/seeds/002-tools.js
+++ data/seeds/002-tools.js
@@ -1,38 +1,38 @@
-exports.seed = function (knex) {
+exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('tools')
+  return knex("tools")
     .truncate()
-    .then(function () {
+    .then(function() {
       // Inserts seed entries
-      return knex('tools').insert([{
-          name: 'saw',
-          username: 'David',
-          borrower_id: 2,
+      return knex("tools").insert([
+        {
+          name: "saw",
+          username: "David",
+          borrower: "Chaz",
           desc: "cuts things"
         },
         {
-          name: 'hammer',
-          username: 'Greg',
-          borrower_id: 3,
+          name: "hammer",
+          username: "Greg",
+          borrower: "David",
           desc: "hits things"
-
         },
         {
-          name: 'drill',
-          username: 'Carlos',
-          borrower_id: 4,
+          name: "drill",
+          username: "Carlos",
+          borrower_id: "Greg",
           desc: "puts holes in things"
         },
         {
-          name: 'handtruck',
-          username: 'Chaz',
-          borrower_id: 1,
+          name: "handtruck",
+          username: "Chaz",
+          borrower: "Carlos",
           desc: "moves heavy things"
         },
         {
-          name: 'wheelbarrow',
-          username: 'David',
-          borrower_id: 4,
+          name: "wheelbarrow",
+          username: "David",
+          borrower: "Jack",
           desc: "carries things"
         }
       ]);
